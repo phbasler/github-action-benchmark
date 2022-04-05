ffea718143f310cb97a320c5fc00cbe6e40f8a38
Philipp Basler <philipp.basler@kit.edu>
2022-04-05 08:58:48 +0200

    Increased runtime to get over 1m

--- examples/catch2/catch2_bench.cpp
+++ examples/catch2/catch2_bench.cpp
@@ -2,10 +2,18 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
+#include <chrono>
+#include <thread>
+
 TEST_CASE("Fibonacci") {
 
+  auto testfunc = [](int i) {
+    std::this_thread::sleep_for(std::chrono::milliseconds(700));
+    return fib(i);
+  };
+
   // now let's benchmark:
-  BENCHMARK("Fibonacci 10") { return fib(10); };
+  BENCHMARK("Fibonacci 10") { return testfunc(10); };
 
   BENCHMARK("Fibonacci 20") { return fib(20); };
 }
